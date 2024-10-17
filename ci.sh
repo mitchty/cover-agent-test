5c8edb976ddb10d89376772314a395c6b181ffc4
Mitchell Tishmack <mitchell.james.tishmack@hpe.com>
2024-10-17 10:15:42 -0500

    make some stuff vars

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ _dir=$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P || exit 126)
 export _base _dir
 set "${SETOPTS:--eu}"
 
-OPENAI_API_KEY= cover-agent \
+OPENAI_API_KEY='' cover-agent \
   --source-file-path calc.py \
   --test-file-path test_calc.py \
   --code-coverage-report-path coverage.xml \
@@ -16,6 +16,6 @@ OPENAI_API_KEY= cover-agent \
   --coverage-type cobertura \
   --desired-coverage 80 \
   --max-iterations 100 \
-  --model ollama/llama3.2 \
-  --api-base http://127.0.0.1:11434 \
+  --model ollama/${MODEL:-llama3.2} \
+  --api-base http://${OLLAMA_HOST}:11434 \
   --additional-instructions "Since I am using a test class, each line of code (including the first line) needs to be prepended with 4 whitespaces. This is extremely important to ensure that every line returned contains that 4 whitespace indent; otherwise, my code will not run. Ensure that all code is indented by 4 whitespaces."
